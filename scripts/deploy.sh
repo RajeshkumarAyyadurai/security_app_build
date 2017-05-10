da4489c14828e726081a9a98818a1b4cf392bf39
s_rayyad <rajesh.ayyadurai@neustar.biz>
2017-05-10 21:37:08 +0530

    change in yml and cfg files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,28 +3,28 @@ if [ $1 -eq "staging" ]; then
 	ansible-playbook /etc/ansible/staging/warDeploy.yml
 	sleep 5m
 	
-	sudo mkdir /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
-	sudo chmod 777 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir
-	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
-	git clone $repo_url
-	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
-	ls -l /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
-	chmod 755 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
-	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
+	sudo mkdir /home/travis/build/RajeshkumarAyyadurai/staging/
+	sudo chmod 777 /home/travis/build/RajeshkumarAyyadurai/staging
+	cd /home/travis/build/RajeshkumarAyyadurai/staging/
+	git clone https://github.com/RajeshkumarAyyadurai/securitytestv1.0.git
+	cd /home/travis/build/RajeshkumarAyyadurai/staging/securitytestv1.0/
+	ls -l /home/travis/build/RajeshkumarAyyadurai/staging/securitytestv1.0/lib/drivers/phantomjs
+	chmod 755 /home/travis/build/RajeshkumarAyyadurai/staging/securitytestv1.0/lib/drivers/phantomjs
+	cd /home/travis/build/RajeshkumarAyyadurai/staging/securitytestv1.0/
 	mvn test
 else
 	cd /etc/ansible
 	ansible-playbook /etc/ansible/prod/warDeploy.yml
 	sleep 5m
 	
-	sudo mkdir /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
-	sudo chmod 777 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir
-	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
-	git clone $repo_url
-	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
-	ls -l /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
-	chmod 755 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
-	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
+	sudo mkdir /home/travis/build/RajeshkumarAyyadurai/prod/
+	sudo chmod 777 /home/travis/build/RajeshkumarAyyadurai/prod
+	cd /home/travis/build/RajeshkumarAyyadurai/prod/
+	git clone https://github.com/RajeshkumarAyyadurai/securitytestv1.0.git
+	cd /home/travis/build/RajeshkumarAyyadurai/prod/securitytestv1.0/
+	ls -l /home/travis/build/RajeshkumarAyyadurai/prod/securitytestv1.0/lib/drivers/phantomjs
+	chmod 755 /home/travis/build/RajeshkumarAyyadurai/prod/securitytestv1.0/lib/drivers/phantomjs
+	cd /home/travis/build/RajeshkumarAyyadurai/prod/securitytestv1.0/
 	mvn test
 fi
 
