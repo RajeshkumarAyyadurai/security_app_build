6eb444b694069a7712b1db7ef0c34a1b03dfbaac
s_rayyad <rajesh.ayyadurai@neustar.biz>
2017-05-10 20:34:00 +0530

    change in yml and cfg files

---
+++ scripts/deploy.sh
@@ -0,0 +1,31 @@
+if [ $1 -eq "staging" ]; then
+	cd /etc/ansible
+	ansible-playbook warDeploy.yml
+	sleep 5m
+	
+	sudo mkdir /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
+	sudo chmod 777 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir
+	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
+	git clone $repo_url
+	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
+	ls -l /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
+	chmod 755 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
+	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
+	mvn test
+else
+	cd /etc/ansible
+	ansible-playbook warDeploy.yml
+	sleep 5m
+	
+	sudo mkdir /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
+	sudo chmod 777 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir
+	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/
+	git clone $repo_url
+	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
+	ls -l /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
+	chmod 755 /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/lib/drivers/phantomjs
+	cd /home/travis/build/RajeshkumarAyyadurai/$deploy_dir/securitytestv1.0/
+	mvn test
+fi
+
+    
