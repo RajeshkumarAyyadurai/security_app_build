7ad197099b74816fa073faacc9a7e382e38f4981
RajeshkumarAyyadurai <yarajesha@gmail.com>
2017-06-21 18:22:41 +0530

    change in IP

--- src/com/DDosSecurity/db/DBIntializer.java
+++ src/com/DDosSecurity/db/DBIntializer.java
@@ -16,7 +16,7 @@ String PASSWORD="oracle";*/
 	
 	/* For MYSQL DATABASE*/
 	String DRIVER="org.postgresql.Driver";
-	String CON_STRING="jdbc:postgresql://54.254.182.212:5432/postgres";
+	String CON_STRING="jdbc:postgresql://13.228.27.227:5432/postgres";
 	String USERNAME="postgres";
 	String PASSWORD="";
 	
